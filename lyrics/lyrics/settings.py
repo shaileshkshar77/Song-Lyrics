759a7e9df4c61f4e121753fea1a9812ecca71158
londo <london.man77@gmail.com>
2019-08-22 21:02:44 +0530

    heroku

--- lyrics/lyrics/settings.py
+++ lyrics/lyrics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^=9wp&25!w@9r0%zq6pkjnq@(kc=pmj15&lny%c$*ge$&os_$#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -122,6 +122,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
