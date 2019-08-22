c639176d34a0d45805a5c84a40e60c5e9725d116
londo <london.man77@gmail.com>
2019-08-22 20:06:48 +0530

    Changes

--- lyrics/songs/models.py
+++ lyrics/songs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Songs(models.Model):
+    title=models.CharField( max_length=50)
+    slug=models.SlugField()
+    body=models.TextField()
+    singer=models.CharField( max_length=50)
+    #add thumbnail
+
+def __str__(self):
+    return self.title
