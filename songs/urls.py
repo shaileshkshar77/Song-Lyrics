304aaf7bca6c03948a40ff69ae79df7759723bd2
londo <london.man77@gmail.com>
2019-08-24 19:34:23 +0530

    few changes

--- songs/urls.py
+++ songs/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     
     path('',views.list),
+    path('(<slug:slug>/',views.article),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import Songs
+from django.http import HttpResponse
 
 def list(request):
     articles=Songs.objects.all()
     return render(request ,"songs/list.html",{"articles":articles})
+
+def article(request,slug):
+    return HttpResponse(slug)
 
 # Create your views here.
