32163570504ff5e89283200e8395ac8a40be467a
londo <london.man77@gmail.com>
2019-09-10 16:28:45 +0530

    Accounts

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+app_name="accounts"
+
+urlpatterns = [
+    path('signup/',views.signup_view),
+]
--- lyrics/urls.py
+++ lyrics/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('songs/',include('songs.urls')),
+    path('accounts/',include('accounts.urls')),
     path('about/',views.about),
     path('',views.home),
 ]
